43b3eb9e96c5da9b2c936dfc82659de1289df210
watasuke102 <watasuke102@gmail.com>
2020-06-17 08:02:03 +0900

    [update] : 起動したら「起動しました」と言うように

--- main.py
+++ main.py
@@ -1,8 +1,45 @@
 #!/usr/bin/env python
 import discord
+from discord.ext import tasks
+from datetime import datetime
+from timetree_sdk import TimeTreeApi
 
 # トークンの読み込み
-file = open('settings')
-TOKEN = file.readline()
-CHANNEL = file.readline()
+file       = open('settings')
+token      = file.readline()
+channelID  = file.readline()
+keyAPI     = file.readline()
+calenderID = file.readline()
 file.close()
+
+print('token: '            + token    )
+print('channel: '          + channelID)
+print('TimeTree API Key: ' + keyAPI   )
+print('Read Settings...Done')
+
+# TimeTree APIの設定
+api      = TimeTreeApi(keyAPI)
+#calender = api.get_calendar(calenderID)
+#print(calender.data.attributes.name)
+
+# 接続用オブジェクト生成
+client  = discord.Client()
+print('Create Client...Done')
+
+@client.event
+async def on_ready():
+	channel = client.get_channel(int(channelID))
+	await channel.send('起動しました')
+
+# 10分ごとに更新
+# 今はテスト用に10秒ごとにしている
+@tasks.loop(seconds=10)
+async def loop():
+	print('Begin of the loop')
+	channel = client.get_channel(int(channelID))
+	await channel.send('こんにちは(send this every 10s)')
+	print('End of the loop')
+
+loop.start()
+print('Running client...')
+client.run(token)
