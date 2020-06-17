10c085eb25314a9949bf329ae981ed2171c5c62b
watasuke102 <watasuke102@gmail.com>
2020-06-17 20:24:45 +0900

    [update] : イベントの詳細を取得するための関数を追加

--- timetree.py
+++ timetree.py
@@ -1,27 +1,55 @@
-from timetree_sdk import TimeTreeApi
 import urllib.request
 import json
+from datetime import datetime
 
 keyAPI     = ''
 calenderID = ''
 
-
-def getTodaysEvents():
+# APIでjsonを取得
+def getEventFromAPI():
 	url = 'https://timetreeapis.com/calendars/{}/upcoming_events?timezone=Asia/Tokyo'.format(calenderID.join(calenderID.split()))
 	req = urllib.request.Request(url)
 	req.add_header('Authorization', 'Bearer ' + keyAPI)
-	todaysEvents = 'おはようございます。'
-	# APIでjsonを取得
 	with urllib.request.urlopen(req) as res:
 		data = json.loads(res.read().decode('UTF-8'))
-		# 予定の件数を取得
-		todaysEvents += '今日の予定は{}件です。\n'.format(len(data['data']))
-		# 予定のタイトルを取得し表示
-		for content in data['data']:
-			todaysEvents += ('・' + content['attributes']['title'] + '：')
-			if content['attributes']['all_day']:
-				todaysEvents += '終日\n'
-			else:
-				todaysEvents += content['attributes']['start_at'][11:16]+'〜'+content['attributes']['end_at'][11:16] + '\n'
+	return data
 
+
+# イベントの開始時間を返す
+def getEventStartAt(content):
+	return content['attributes']['start_at'][11:16]
+
+# イベントの終了時間を返す
+def getEventEndAt(content):
+	return content['attributes']['start_at'][11:16]
+
+# イベントの名前を返す
+def getEventTitle(content):
+	content['attributes']['all_day']
+
+# その日のイベントを取得し、一覧にした文字列を返す
+def getTodaysEvents():
+	data = getEventFromAPI()
+	# 予定の件数を取得
+	todaysEvents = 'おはようございます。今日の予定は{}件です。\n\n'.format(len(data['data']))
+	# 予定のタイトルを取得し表示
+	for content in data['data']:
+		todaysEvents += ('・' + getEventTitle(content) + '：')
+		if content['attributes']['all_day']:
+			todaysEvents += '終日\n'
+		else:
+			todaysEvents += getEventStartAt(content) + '〜' + getEventEndAt(content) + '\n'
 	return todaysEvents
+
+
+# その日のイベントを取得し、現在時刻と比べる
+# 10分後に開始されるイベントがあればそのイベント名を返す
+def getEventAfterTenMinutes():
+	data  = getEventFromAPI()
+	event = ''
+	a = datetime.now() + datetime.timedelta(minutes=10)
+	now = a.strftime('%H:%M')
+	for content in data['data']:
+		if getEventStartAt(content) == now:
+			event += getEventTitle(content)
+			print(getEventTitle(content)+' | ' + now+'(now):(start)'+getEventStartAt(content))
