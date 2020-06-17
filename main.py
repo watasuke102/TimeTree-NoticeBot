47cbae66630383856baa5e38421437b3bfe97d07
watasuke102 <watasuke102@gmail.com>
2020-06-17 15:39:04 +0900

    [update] : 予定を取得できるように

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
 #!/usr/bin/env python
-import discord
+#coding:utf-8
 import timetree
+import discord
 from discord.ext  import tasks
 from datetime     import datetime
 
-# トークンの読み込み
-file       = open('settings')
-token      = file.readline()
-channelID  = file.readline()
-keyAPI     = file.readline()
-calenderID = file.readline()
-file.close()
+# トークン等の読み込み
+with open('settings', 'r') as file:
+	token      = file.readline()
+	channelID  = file.readline()
+	keyAPI     = file.readline()
+	calenderID = file.readline()
+	# 改行の削除
+	token     =''.join(token     .split())
+	channelID =''.join(channelID .split())
+	keyAPI    =''.join(keyAPI    .split())
+	calenderID=''.join(calenderID.split())
 
-print(
- 'token: '            + token
-+'channel: '          + channelID
-+'TimeTree API Key: ' + keyAPI)
-print('Read Settings from file...Done')
+print('token: '            + token     )
+print('channel: '          + channelID )
+print('TimeTree API Key: ' + keyAPI    )
+print('calender ID: '      + calenderID)
+print('> Read Settings from file...Done')
 
 # TimeTree APIの設定
-timetree.init(keyAPI)
+timetree.keyAPI     = keyAPI
+timetree.calenderID = calenderID
+print('---EVENTS---')
+timetree.getTodaysEvents()
+print('---EVENTS END---')
 
 # 接続用オブジェクト生成
 client  = discord.Client()
-print('Create client...Done')
+print('> Create client...Done')
 
 
 # 10分ごとに更新
@@ -32,16 +41,15 @@ print('Create client...Done')
 async def loop():
 	print('-- Begin of the loop --')
 	channel = client.get_channel(int(channelID))
-	print('Get Channel...Done')
+	print('  > Get Channel...Done')
 	# 現在時刻を取得
 	now = datetime.now().strftime('%H:%M')
-	print('Get time...Done\nnow: ' + now)
+	print('  > Get time...Done\nnow: ' + now)
 	# 8時ならおはようする
 	# デバッグのため時間を変えてる
 	if now == '13:11':
 		print('NOW')
 		await channel.send('おはよう')
-	print('Send Message')
 	print('-- End of the loop --\n')
 
 
--- timetree.py
+++ timetree.py
@@ -1,5 +1,14 @@
 from timetree_sdk import TimeTreeApi
+import urllib.request
+import json
+
+keyAPI     = ''
+calenderID = ''
 
 
-def init(api_temp):
-	keyAPI = api_temp
+def getTodaysEvents():
+	url = 'https://timetreeapis.com/calendars/{}/upcoming_events?timezone=Asia/Tokyo'.format(calenderID.join(calenderID.split()))
+	req = urllib.request.Request(url)
+	req.add_header('Authorization', 'Bearer ' + keyAPI)
+	with urllib.request.urlopen(req) as res:
+		print(res.read().decode('UTF-8'))
