4eeec31b386ee44dce0e5c3515eb4415cc6b1885
watasuke102 <watasuke102@gmail.com>
2020-06-17 16:06:08 +0900

    [update] : getTodaysEvents()が 今日の予定 を取得して返すように

--- timetree.py
+++ timetree.py
@@ -10,5 +10,11 @@ def getTodaysEvents():
 	url = 'https://timetreeapis.com/calendars/{}/upcoming_events?timezone=Asia/Tokyo'.format(calenderID.join(calenderID.split()))
 	req = urllib.request.Request(url)
 	req.add_header('Authorization', 'Bearer ' + keyAPI)
+	todaysEvents = 'おはようございます。'
+	# APIでjsonを取得
 	with urllib.request.urlopen(req) as res:
-		print(res.read().decode('UTF-8'))
+		data = json.loads(res.read().decode('UTF-8'))
+		todaysEvents += '今日の予定は{}件です。\n'.format(len(data['data']))
+		for content in data['data']:
+			todaysEvents += ('・' + content['attributes']['title'] + '\n')
+	return todaysEvents
